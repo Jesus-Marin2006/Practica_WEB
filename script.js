ed21a1a7a410196cf98d15d0a476c1736354e5fe
Jesus-Marin2006 <jesusmarinenriquez3@gmail.com>
2025-05-06 09:17:56 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+
+// --------------------FUNCION 1---------------------
+/*
+function aleatorio(){
+    let numero= Math.random(); 
+    if(numero <=.5){
+        document.writeln(numero+ "Menor que 0.5");
+    }
+    else{
+        document.writeln(numero+ "Mayor que 0.5")
+    }
+}
+*/
+
+// --------------------FUNCION 2---------------------
+/*
+function edad(){
+    let edadP= document.getElementById("edadP").value;
+
+    let resultadoDiv= document.getElementById("resultadoEdad")
+    
+    
+        if (edadP <= 17 ){
+            resultadoDiv.textContent="Tu edad es menor " + edadP
+            // document.getElementById("resultadoEdad").innerHTML= "Menor de edad"
+        }else{
+            resultadoDiv.textContent="Tu edad es mayor " + edadP
+            // document.getElementById("resultadoEdad").innerHTML= "Mayor de edad"
+        }
+         
+}
+*/
+// --------------------FUNCION 3---------------------
+/*
+function edad() {
+let edadP= document.getElementById("edadP").value;
+let resultadoDiv= document.getElementById("resultadoEdad");
+
+if (edadP < 18) {
+    resultadoDiv.textContent="Alice es menor de 18 años."
+    // document.getElementById("resultadoEdad").innerHTML="Alice es menor de 18 años.";
+  } else if (edadP >= 18 && edadP <= 21) {
+    resultadoDiv.textContent="Alice tiene entre 18 y 21 años de edad."
+    // document.getElementById("resultadoEdad").innerHTML= "Alice tiene entre 18 y 21 años de edad.";
+  } else { 
+    resultadoDiv.textContent="Alice tiene mas de 21 años."
+    // document.getElementById("resultadoEdad").innerHTML= "Alice tiene mas de 21 años.";
+  }
+}
+*/
